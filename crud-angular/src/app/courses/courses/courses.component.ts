3cd85a85d6c087fca236974743300488532d942c
Thiago Bernardi Parascandalo <thiago520@gmail.com>
2022-05-03 17:01:51 -0300

    first commit

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,27 @@
+import { CoursesService } from './../services/courses.service';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+    //this.coursesService = new CoursesService();
+    this.courses$ = this.coursesService.list();
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
