d2cfbd1889be2cdb809cd634fefd7c9851084516
Thiago Bernardi Parascandalo <thiago520@gmail.com>
2022-05-10 19:38:39 -0300

    adicionado pagina formularios e botoes

--- crud-spring/src/main/java/com/thiago/controller/CourseController.java
+++ crud-spring/src/main/java/com/thiago/controller/CourseController.java
@@ -3,9 +3,9 @@ package com.thiago.controller;
 import com.thiago.model.Course;
 import com.thiago.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,12 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+        return courseRepository.save(course);
     }
 }
