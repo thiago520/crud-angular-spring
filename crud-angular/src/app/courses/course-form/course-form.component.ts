2d8c6c51b190b3dfba276d9f03478b09033911fd
Thiago Bernardi Parascandalo <thiago520@gmail.com>
2022-08-16 22:56:22 -0300

    update angular

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from './../services/courses.service';
@@ -10,27 +10,28 @@ import { CoursesService } from './../services/courses.service';
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss']
 })
+
 export class CourseFormComponent implements OnInit {
 
-  form: UntypedFormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
-  constructor(private formBuilder: UntypedFormBuilder,
-              private service: CoursesService,
-              private snackBar: MatSnackBar,
-              private location: Location) {
+  constructor(private formBuilder: NonNullableFormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location) {
 
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
   }
+
 
   ngOnInit(): void {
   }
 
   onSubmit() {
     this.service.save(this.form.value)
-      .subscribe(result => this.onSucess(), error => this.onError() );
+      .subscribe(result => this.onSucess(), error => this.onError());
   }
 
   onCancel() {
--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -24,7 +24,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatSnackBarModule
+    MatSnackBarModule,    
   ],
 
 })
